f4e9e12a2c2f6af997a22fa3dbdb49caa5f067c6
PenGuinMike <104021074@live.asia.edu.tw>
2017-10-03 12:31:00 +0800

    save

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MainFrame mFme=new MainFrame();
+        mFme.setVisible(true);
+
+
+    }
+}
